7da6054180bab1f04faa3b45572b945db1bc7447
Y.Ukrainets <y.ukrainets@viseven.com>
2021-02-17 23:44:05 +0200

    upd(*):add components

--- client/src/components/contact-list.vue
+++ client/src/components/contact-list.vue
@@ -1,19 +1,49 @@
 <template>
-  <v-container>
-    <v-text >{{this.contacts}}</v-text>
-  </v-container>
+  <div>
+  <v-text-field v-model="search"></v-text-field>
+  <v-data-table
+    :headers="headers"
+    :items="contacts"
+    :items-per-page="5"
+    :search="search"
+    :loading="preloader"
+    loading-text="Loading... Please wait"
+    class="elevation-1"
+  ></v-data-table>
+  </div>
 </template>
-
 <script>
+
 export default {
-  name: 'HelloWorld',
-
+  name: 'contact-list',
   data: () => ({
-
+    search: '',
+    headers: [
+      {
+        text: 'First name',
+        align: 'start',
+        sortable: false,
+        value: 'first_name',
+      },
+      {
+        text: 'Last name',
+        align: 'start',
+        sortable: false,
+        value: 'last_name',
+      },
+      {
+        text: 'Number',
+        sortable: false,
+        value: 'number',
+      },
+    ],
   }),
-  computed: {
-    contacts() {
-      return this.$store.getters.contacts;
+  props: {
+    contacts: {
+      type: Array,
+    },
+    preloader: {
+      type: Boolean,
     },
   },
 };
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,10 +5,46 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    contacts: [
+      {
+        _id: {
+          $oid: '602d7269f7e04204b3f1fc4d',
+        },
+        number: '0971234585',
+        first_name: 'John',
+        last_name: 'Doe',
+      }, {
+        _id: {
+          $oid: '602d727ef7e04204b3f1fc4e',
+        },
+        number: '1218888888',
+        first_name: 'Michael',
+        last_name: 'Jackson',
+      }, {
+        _id: {
+          $oid: '602d729af7e04204b3f1fc4f',
+        },
+        number: '1218889928',
+        first_name: 'Flash',
+        last_name: 'Gordon',
+      }, {
+        _id: {
+          $oid: '602d769af7e04204b2f1fc4f',
+        },
+        number: '1558449887',
+        first_name: 'Clark',
+        last_name: 'Kent',
+      },
+    ],
+    preloader: false,
   },
   mutations: {
   },
   actions: {
+  },
+  getters: {
+    contacts: (state) => state.contacts,
+    preloader: (state) => state.preloader,
   },
   modules: {
   },
