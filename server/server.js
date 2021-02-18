394ab13956ec663b6188acd7d8c6da3ee959dad3
Y.Ukrainets <y.ukrainets@viseven.com>
2021-02-18 23:26:31 +0200

    upd(server):add endpoints, db connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const cors = require('cors');
 const clientUrl = 'http://localhost:8081';
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
+const jsonParser = express.json();
 
 const corsOptions = {
   origin: clientUrl,
@@ -11,21 +14,89 @@ const port = 3000;
 
 const app = express();
 app.use(cors());
-app.all('/*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', clientUrl);
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  res.send('Hello!')
-  next();
-});
-app.get('/getProducts', cors(corsOptions), (req, res) => {
-  products.getContent
-    .then(data => res.json(data), err => res.json(err));
-});
-app.get('/getUsers', cors(corsOptions), (req, res) => {
-  db.getUsers()
-    .then(data => res.json(data), err => res.json(err));
+const mongoClient = new MongoClient('mongodb://localhost:27017/', { useUnifiedTopology: true });
+
+let dbClient;
+
+mongoClient.connect(function (err, client) {
+  if (err) return console.log(err);
+  dbClient = client;
+  app.locals.collection = client.db('telephone-book')
+    .collection('contacts');
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/api/contacts', function (req, res) {
+  const collection = req.app.locals.collection;
+  collection.find()
+    .toArray(function (err, users) {
+      if (err) return console.log(err);
+      res.send(users);
+    });
+});
+
+app.get('/api/contacts/:id', function (req, res) {
+  const id = new objectId(req.params.id);
+  const collection = req.app.locals.collection;
+  collection.findOne({ _id: id }, function (err, user) {
+    if (err) return console.log(err);
+    res.send(user);
+  });
+});
+
+app.post("/api/contacts", jsonParser, function (req, res) {
+
+  if(!req.body) return res.sendStatus(400);
+
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const number = req.body.number;
+  const contact = {firstName: firstName, lastName: lastName,number:number, isFavourite:false};
+
+  const collection = req.app.locals.collection;
+  collection.insertOne(contact, function(err, result){
+    if(err) return console.log(err);
+    res.send(user);
+  });
+});
+
+app.put("/api/contacts", jsonParser, function(req, res){
+
+  if(!req.body) return res.sendStatus(400);
+  const id = new objectId(req.body.id);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const number = req.body.number;
+  const isFavourite = req.body.isFavourite;
+  const contact = {firstName: firstName, lastName: lastName,number:number, isFavourite:isFavourite};
+
+  const collection = req.app.locals.collection;
+  collection.findOneAndUpdate({_id: id}, { $set: contact},
+    {returnOriginal: false },function(err, result){
+      if(err) return console.log(err);
+      const user = result.value;
+      res.send(user);
+    });
+});
+
+app.delete("/api/contacts/:id", function(req, res){
+
+  const id = new objectId(req.params.id);
+  const collection = req.app.locals.collection;
+  collection.findOneAndDelete({_id: id}, function(err, result){
+    if(err) return console.log(err);
+    let user = result.value;
+    res.send(user);
+  });
+});
+
+
+
+process.on("SIGINT", () => {
+  console.log('Start shutdown...');
+  dbClient.close();
+  console.log('Server down!');
+  process.exit();
+});
